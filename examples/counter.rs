d1eca3851c86e2a82931e07f5ff37ca5437d2afb
Hennadii Chernyshchyk <genaloner@gmail.com>
2022-07-11 23:13:50 +0300

    Add simple example and setup CI
    I added litex-sim-pac crate to the workspace to be able to provide
    the example and test macros compilation.

---
+++ examples/counter.rs
@@ -0,0 +1,32 @@
+#![no_std]
+#![no_main]
+
+use core::fmt::Write;
+use litex_hal::prelude::*;
+use litex_sim_pac::{riscv_rt::entry, Peripherals};
+use panic_halt as _;
+
+litex_hal::uart! {
+    Uart: litex_sim_pac::UART,
+}
+
+litex_hal::timer! {
+    Timer: litex_sim_pac::TIMER0,
+}
+
+const SYSTEM_CLOCK_FREQUENCY: u32 = 1_000_000;
+
+#[entry]
+fn main() -> ! {
+    let peripherals = Peripherals::take().unwrap();
+    let mut uart = Uart::new(peripherals.UART);
+    writeln!(uart, "Peripherals initialized").unwrap();
+
+    let mut timer = Timer::new(peripherals.TIMER0, SYSTEM_CLOCK_FREQUENCY);
+    let mut uptime = 0;
+    loop {
+        timer.delay_ms(1000_u32);
+        uptime += 1;
+        writeln!(uart, "Uptime: {} seconds", uptime).unwrap();
+    }
+}
